d97047f327dd5ea255dd317f67f8ad37ebd3304e
alexTU1 <95954794+alexTU1@users.noreply.github.com>
2024-09-25 21:07:32 +0000

    created matrix with nested for loops

---
+++ forloop.tcl
@@ -0,0 +1,17 @@
+#Syntax:
+#for {set variable} {condition} {incrementation}
+
+puts "Enter a number for your matrix:"
+set matrix [gets stdin]
+puts "\nOkay, you chose a $matrix x $matrix matrix\n"
+
+puts "Configuring..."
+after 1200
+
+for {set i 0} {$i < $matrix} {set i [expr $i + 1]} {
+    for {set j 0} {$j < $matrix} {set j [expr $j + 1]} {
+        puts -nonewline "*    "
+        after 50
+    }
+    puts \n
+}
