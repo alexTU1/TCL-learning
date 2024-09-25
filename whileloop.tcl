c276df30de28a6b1f17c09c7eb24159ed28b27dc
alexTU1 <95954794+alexTU1@users.noreply.github.com>
2024-09-25 20:02:43 +0000

    making it interactive

--- whileloop.tcl
+++ whileloop.tcl
@@ -2,9 +2,15 @@ set x 0.0
 #setting decimal # of places
 set tcl_precision 2
 
+puts "Type counter value (integer):"
+set range_cap [gets stdin];#user input for counter number
+
+puts "Type increment value:"
+set interval [gets stdin];#user input for interval
+
 #incrementing x by 0.2 until it reaches 5
-while {$x < 5} {
-    set x [expr $x + 0.2]
+while {$x < $range_cap} {
+    set x [expr $x + $interval]
     puts "x is: $x"
     #waiting for 50 miliseconds ==> 0.05 seconds
     after 50
